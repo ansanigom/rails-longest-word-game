76f40892a89926127a00e1a26ed0696db04e1d58
Anju <88181896+ansanigom@users.noreply.github.com>
2021-12-08 07:39:14 +0900

    changed database to pg

--- app/controllers/games_controller.rb
+++ app/controllers/games_controller.rb
@@ -5,31 +5,29 @@ class GamesController < ApplicationController
   end
 
   def english_word?
-    url = "https://wagon-dictionary.herokuapp.com/#{@answer.downcase}"
+    url = "https://wagon-dictionary.herokuapp.com/#{@word.downcase}"
     dictionary_call = URI.open(url).read
     json = JSON.parse(dictionary_call)
     return json['found']
   end
 
-  def made_of?
-    @answer = params[:word]
-    split_answer = @answer.split
-    split_answer.all? do |letter|
-      @letters.include? letter
+  def made_of?(letters, word)
+    word.upcase.chars.all? do |letter|
+      word.upcase.count(letter) <= word.count(letter)
     end
   end
 
   def score
-    @answer = params[:word]
+    @word = params[:word]
     @letters = params[:letters]
-    if english_word?
-      @result = "Congratulations!#{@answer} is a valid English word!"
-    else !english_word?
-      @result = "Sorry, #{@answer} does not seem to be a valid English word.."
-    end
-
-    if made_of?
-      @result = "Sorry, #{@answer} can't be build out of #{@letters}"
+    unless made_of?(@letters, @word)
+      if english_word?
+        @result = "Congratulations! #{@word.upcase} is a valid English word!"
+      else !english_word?
+        @result = "Sorry, #{@word.upcase} does not seem to be a valid English word.."
+      end
+    else
+      @result = "Sorry, #{@word.upcase} can't be built."
     end
   end
 end
